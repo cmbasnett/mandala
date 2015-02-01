1e56551d86f35ecd5335e1ddfed2f2a42de6b70a
Colin Basnett <cmbasnett@gmail.com>
2015-02-01 15:18:27 -0800

    fix for build errors

--- mandala/gui_scroll.cpp
+++ mandala/gui_scroll.cpp
@@ -10,7 +10,7 @@ namespace mandala
 {
     void gui_scroll_t::render(mat4_t world_matrix, mat4_t view_projection_matrix)
     {
-        world_matrix *= glm::translate(world_matrix, vec3_t(_scroll_position, 0));
+        world_matrix *= glm::translate(world_matrix, vec3_t(scroll_position, 0));
 
         gui_node_t::render(world_matrix, view_projection_matrix);
     }
@@ -20,7 +20,7 @@ namespace mandala
         //TODO: have some way for layouts to keep track of certain nodes 'owning' touch events by id so that they get exclusive rights to handle future touch events of the same id
 		if (input_event.device_type == input_event_t::device_type_e::touch)
 		{
-			input_event.touch.position -= _scroll_position;
+			input_event.touch.position -= scroll_position;
 		}
 
 		gui_node_t::on_input_event(input_event);
@@ -32,7 +32,7 @@ namespace mandala
 		
 		if (input_event.device_type == input_event_t::device_type_e::touch)
 		{
-			input_event.touch.position += _scroll_position;
+			input_event.touch.position += scroll_position;
 		}
 
         switch (input_event.touch.type)
@@ -40,26 +40,26 @@ namespace mandala
         case input_event_t::touch_t::type_e::press:
             if (contains(get_bounds(), input_event.touch.position))
             {
-                _is_scrolling = true;
-                _touch_id = input_event.touch.id;
+                is_scrolling = true;
+                touch_id = input_event.touch.id;
 
 				input_event.is_consumed = true;
             }
             break;
         case input_event_t::touch_t::type_e::release:
-            if (_is_scrolling && _touch_id == input_event.touch.id)
+            if (is_scrolling && touch_id == input_event.touch.id)
             {
-                _is_scrolling = false;
-				_touch_id = 0;
+                is_scrolling = false;
+				touch_id = 0;
 
 				input_event.is_consumed = true;
             }
             break;
         case input_event_t::touch_t::type_e::move:
-            if (_is_scrolling && _touch_id == input_event.touch.id)
+            if (is_scrolling && touch_id == input_event.touch.id)
             {
-                _scroll_position.x += static_cast<float32_t>(input_event.touch.position_delta.x);
-				_scroll_position.y -= static_cast<float32_t>(input_event.touch.position_delta.y);
+                scroll_position.x += static_cast<float32_t>(input_event.touch.position_delta.x);
+				scroll_position.y -= static_cast<float32_t>(input_event.touch.position_delta.y);
 
 				input_event.is_consumed = true;
             }
@@ -69,7 +69,7 @@ namespace mandala
 
     void gui_scroll_t::tick(float32_t dt)
     {
-        _scroll_position = glm::clamp(_scroll_position, _scroll_extents.min, _scroll_extents.max);
+        scroll_position = glm::clamp(scroll_position, scroll_extents.min, scroll_extents.max);
 
         gui_node_t::tick(dt);
     }
