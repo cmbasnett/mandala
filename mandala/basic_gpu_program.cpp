2febb5358cc298b7894efe0c9e239bdfd140a859
Colin Basnett <cmbasnett@gmail.com>
2015-02-08 06:47:00 -0800

    replacing more instances of 'position' with 'location'

--- mandala/basic_gpu_program.cpp
+++ mandala/basic_gpu_program.cpp
@@ -12,14 +12,14 @@ namespace mandala
 uniform mat4 world_matrix;
 uniform mat4 view_projection_matrix;
 
-in vec3 position;
+in vec3 location;
 in vec4 color;
 
 out vec4 out_color;
 
 void main() 
 {
-	gl_Position = view_projection_matrix * (world_matrix * vec4(position, 1));
+	gl_Position = view_projection_matrix * (world_matrix * vec4(location, 1));
 
 	out_color = color;
 })";
