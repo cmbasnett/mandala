09ef8f31ad9826bfbe0bd133d3e67b26a46b508d
Colin Basnett <cmbasnett@gmail.com>
2015-02-08 09:28:01 -0800

    dumb user defined literal

---
+++ mandala/literals.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+//std
+#include <ratio>
+
+namespace mandala
+{
+    constexpr long double operator"" _deg(long double deg)
+    {
+        return deg * (3.141592 / 180);
+    }
+
+    constexpr long double operator"" _rad(long double rad)
+    {
+        return rad * (180 / 3.141592);
+    }
+
+    constexpr auto d = 360.0_deg;
+    constexpr auto r = 1.432_rad;
+}
