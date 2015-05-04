b634a0503b9fb6a035bcac2e976ff2bfcff540b8
Colin Basnett <cmbasnett@gmail.com>
2015-05-04 14:12:02 -0700

    quick and dirty sphere renderer

--- mandala/line_renderer.hpp
+++ mandala/line_renderer.hpp
@@ -8,6 +8,7 @@
 #include "basic_gpu_program.hpp"
 #include "rectangle.hpp"
 #include "line.hpp"
+#include "sphere.hpp"
 
 #include "gpu_buffer_mgr.hpp"
 
@@ -100,6 +101,83 @@ namespace mandala
         gpu.set_uniform("view_projection_matrix", view_projection_matrix);
 
         gpu.draw_elements(gpu_t::primitive_type_e::lines, 24, index_buffer_type::data_type, 0);
+
+        gpu.programs.pop();
+
+        gpu.buffers.pop(gpu_t::buffer_target_e::element_array);
+        gpu.buffers.pop(gpu_t::buffer_target_e::array);
+    }
+
+    template<typename T>
+    void render_sphere(const mat4_t& world_matrix, const mat4_t& view_projection_matrix, const details::sphere_t<T>& sphere)
+    {
+        typedef vertex_buffer_t<basic_gpu_vertex_t> vertex_buffer_type;
+        typedef index_buffer_t<uint8_t> index_buffer_type;
+
+        static boost::weak_ptr<vertex_buffer_type> vertex_buffer;
+        static boost::weak_ptr<index_buffer_type> index_buffer;
+
+        const size_t SPHERE_SIDES = 32;
+
+        if (vertex_buffer.expired())
+        {
+            vertex_buffer = gpu_buffers.make<vertex_buffer_type>();
+
+            std::vector<basic_gpu_vertex_t> vertices;
+            vertices.reserve(SPHERE_SIDES);
+
+            for (auto i = 0; i < SPHERE_SIDES; ++i)
+            {
+                auto sigma = static_cast<float32_t>(i) / SPHERE_SIDES * glm::pi<float32_t>() * 2;
+
+                vertices.emplace_back(basic_gpu_vertex_t::location_type(glm::sin(sigma), glm::cos(sigma), 0), basic_gpu_vertex_t::color_type(0, 0, 1, 1), basic_gpu_vertex_t::texcoord_type());
+            }
+
+            for (auto i = 0; i < SPHERE_SIDES; ++i)
+            {
+                auto sigma = static_cast<float32_t>(i) / SPHERE_SIDES * glm::pi<float32_t>() * 2;
+
+                vertices.emplace_back(basic_gpu_vertex_t::location_type(0, glm::sin(sigma), glm::cos(sigma)), basic_gpu_vertex_t::color_type(1, 0, 0, 1), basic_gpu_vertex_t::texcoord_type());
+            }
+
+            for (auto i = 0; i < SPHERE_SIDES; ++i)
+            {
+                auto sigma = static_cast<float32_t>(i) / SPHERE_SIDES * glm::pi<float32_t>() * 2;
+
+                vertices.emplace_back(basic_gpu_vertex_t::location_type(glm::cos(sigma), 0, glm::sin(sigma)), basic_gpu_vertex_t::color_type(0, 1, 0, 1), basic_gpu_vertex_t::texcoord_type());
+            }
+
+            vertex_buffer.lock()->data(vertices, gpu_t::buffer_usage_e::dynamic_draw);
+        }
+
+        if (index_buffer.expired())
+        {
+            index_buffer = gpu_buffers.make<index_buffer_type>();
+
+            std::vector<uint8_t> indices;
+            indices.reserve(SPHERE_SIDES * 3);
+
+            for (auto i = 0; i < SPHERE_SIDES * 3; ++i)
+            {
+                indices.push_back(i);
+            }
+
+            index_buffer.lock()->data(indices, gpu_t::buffer_usage_e::static_draw);
+        }
+
+        gpu.buffers.push(gpu_t::buffer_target_e::array, vertex_buffer.lock());
+        gpu.buffers.push(gpu_t::buffer_target_e::element_array, index_buffer.lock());
+
+        const auto gpu_program = gpu_programs.get<basic_gpu_program_t>();
+
+        gpu.programs.push(gpu_program);
+
+        gpu.set_uniform("world_matrix", world_matrix * glm::translate(sphere.origin) * glm::scale(vec3_t(sphere.radius)));
+        gpu.set_uniform("view_projection_matrix", view_projection_matrix);
+
+        gpu.draw_elements(gpu_t::primitive_type_e::line_loop, SPHERE_SIDES, index_buffer_type::data_type, SPHERE_SIDES * 0);
+        gpu.draw_elements(gpu_t::primitive_type_e::line_loop, SPHERE_SIDES, index_buffer_type::data_type, SPHERE_SIDES * 1);
+        gpu.draw_elements(gpu_t::primitive_type_e::line_loop, SPHERE_SIDES, index_buffer_type::data_type, SPHERE_SIDES * 2);
 
         gpu.programs.pop();
 
